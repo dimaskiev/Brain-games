f355469ec1378ae2b0358506b731ab2519266ffa
dimaskiev <manager.ua@gmail.com>
2017-10-31 15:12:58 +0200

    fix bug eslint

--- src/index.js
+++ src/index.js
@@ -10,43 +10,49 @@ const getName = () => {
 };
 
 const getNumber = () => {
-  const num = Math.floor(Math.random(1)*50);
+  const num = Math.floor(Math.random(1) * 50);
   return num;
   // return num;
-}
+};
 
-const itsEven = (num) =>((num%2)===0)?true:false;
+const itsEven = (num) => {
+  if ((num % 2) === 0) return true;
+  return false;
+};
 
-const correctAnswers = (iseven, Answer) =>{
-  if ((iseven===true)&&(Answer==='yes')) return true;
-   else if ((iseven===false)&&(Answer==='no'))  return true;
-    else return false;
-}
+const correctAnswers = (iseven, Answer) => {
+  if ((iseven === true) && (Answer === 'yes')) {
+    return true;
+  } else if ((iseven === false) && (Answer === 'no')) {
+    return true;
+  }
+  return false;
+};
 
 export const isEven = () => {
   welcome();
   console.log('Answer "yes" if number even otherwise answer "no".\n');
   const userName = getName();
-  let rightAnswer=0, countAnswer = 1;
-  
-  while(countAnswer<=3){
-    const number=getNumber();
-    const iseven=itsEven(number);
-    const correct = (iseven===true)?'yes':'no';
+  let rightAnswer = 0;
+  let countAnswer = 1;
+  while (countAnswer <= 3) {
+    const number = getNumber();
+    const iseven = itsEven(number);
+    const correct = (iseven === true) ? 'yes' : 'no';
     console.log(`Question: ${number}`);
     const Answer = readlineSync.question('Answer: ');
-    let isTrue = correctAnswers(iseven, Answer);
-    if (isTrue){
+    const isTrue = correctAnswers(iseven, Answer);
+    if (isTrue) {
       console.log('Correct!');
-      rightAnswer++;
-      countAnswer++;
+      rightAnswer += 1;
+      countAnswer += 1;
     } else {
       console.log(`"${Answer}" is wrong answer ;(. Correct answer was "${correct}"`);
       console.log(`Let's try again, ${userName}`);
       return;
     }
   }
-  if (rightAnswer===3) console.log(`Congratulations, ${userName}`);
+  if (rightAnswer === 3) console.log(`Congratulations, ${userName}`);
 };
 
 
